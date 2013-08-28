f5deaa7500f018a57c368541ef2fb351bdca3c7a
Alberto De Bortoli <albertodebortoli@gmail.com>
2013-08-28 13:08:40 +0100

    Added CTXTTLManager.

---
+++ CTXTTLManager.m
@@ -0,0 +1,157 @@
+//
+//  CTXTTLManager.m
+//  SMARTClassroom
+//
+//  Created by Pedro Gomes on 29/11/2012.
+//  Copyright (c) 2012 EF Education First. All rights reserved.
+//
+
+#import "CTXTTLManager.h"
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+@interface CTXTTLManager()
+
+- (void)_cancelTimerForObject:(id)object;
+
+@end
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+@implementation CTXTTLManager
+{
+    dispatch_queue_t    _lockQueue;
+    NSMutableArray      *_objects;
+    NSMutableArray      *_timers;
+}
+
+#pragma mark - Dealloc and Initialization
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (void)dealloc
+{
+    [self _performCleanup];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (id)initWithDelegate:(id<CTXTTLManagerDelegate>)delegate
+{
+    if((self = [self init])) {
+        _delegate = delegate;
+    }
+    return self;
+}
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (id)init
+{
+    if((self = [super init])) {
+        _lockQueue  = dispatch_queue_create("com.ef.ctx.ttl-manager.lock", NULL);
+        _objects    = [[NSMutableArray alloc] init];
+        _timers     = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+#pragma mark - Public Methods
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (void)addObject:(id)object ttl:(NSTimeInterval)ttl
+{
+    dispatch_sync(_lockQueue, ^{
+//        NSAssert([_objects indexOfObject:object] == NSNotFound, @"object %@ already exists", object);
+        if([_objects indexOfObject:object] != NSNotFound) {
+            return;
+        }
+        
+        [_objects addObject:object];
+        
+        NSObject<OS_dispatch_source> *timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, _lockQueue);
+        [_timers addObject:timer];
+        
+        dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, ttl * NSEC_PER_SEC), DISPATCH_TIME_FOREVER, 0);
+        dispatch_source_set_event_handler(timer, ^{
+            [self _cancelTimerForObject:object];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self.delegate ttlForObjectExpired:object];
+            });
+        });
+        dispatch_resume(timer);
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (BOOL)updateObject:(id)object ttl:(NSTimeInterval)ttl
+{
+    __block BOOL updated = NO;
+    dispatch_sync(_lockQueue, ^{
+        NSUInteger indexOfObject = [_objects indexOfObject:object];
+        if(indexOfObject != NSNotFound) {
+            NSObject<OS_dispatch_source> *timer = [_timers objectAtIndex:indexOfObject];
+            dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, ttl * NSEC_PER_SEC), DISPATCH_TIME_FOREVER, 0);
+            updated = YES;
+        }
+    });
+    return updated;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (void)removeObject:(id)object
+{
+    dispatch_sync(_lockQueue, ^{
+        [self _cancelTimerForObject:object];
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (void)removeAllObjects
+{
+    dispatch_sync(_lockQueue, ^{ @autoreleasepool {
+            NSArray *objectsToRemove = [NSArray arrayWithArray:_objects];
+            [objectsToRemove enumerateObjectsUsingBlock:^(id object, NSUInteger idx, BOOL *stop) {
+                [self _cancelTimerForObject:object];
+            }];
+        }
+    });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Needs to be wrapped with the appropriate locking mechanism
+////////////////////////////////////////////////////////////////////////////////
+- (void)_cancelTimerForObject:(id)object
+{
+    NSUInteger indexOfObject = [_objects indexOfObject:object];
+//    NSAssert(indexOfObject != NSNotFound, @"object %@ does not exist", object);
+    
+    if(indexOfObject == NSNotFound) {
+        return;
+    }
+    
+    NSObject<OS_dispatch_source> *timer = [_timers objectAtIndex:indexOfObject];
+    dispatch_source_cancel(timer);
+    
+    [_objects removeObjectAtIndex:indexOfObject];
+    [_timers removeObjectAtIndex:indexOfObject];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+- (void)_performCleanup
+{
+    dispatch_sync(_lockQueue, ^{ @autoreleasepool {
+        [_timers enumerateObjectsUsingBlock:^(NSObject<OS_dispatch_source> *timer, NSUInteger idx, BOOL *stop) {
+            dispatch_source_cancel(timer);
+        }];
+        [_timers removeAllObjects];
+        [_objects removeAllObjects];
+    }
+    });
+}
+
+@end
