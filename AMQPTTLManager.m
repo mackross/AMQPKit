0e454150eb0b5f90389d6b0eacf0da4c39891e0d
Alberto De Bortoli <albertodebortoli@gmail.com>
2014-01-13 15:31:37 +0000

    Created Xcode project.

--- AMQPTTLManager.m
+++ AMQPTTLManager.m
@@ -52,7 +52,7 @@
         [_objects addObject:object];
         
         dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, _lockQueue);
-        [_timers addObject:[NSValue valueWithPointer:timer]];
+        [_timers addObject:[NSValue valueWithPointer:(__bridge const void *)(timer)]];
         
         dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, ttl * NSEC_PER_SEC), DISPATCH_TIME_FOREVER, 0);
         dispatch_source_set_event_handler(timer, ^{
---
+++ librabbitmq-objc-demo/librabbitmq-objc-demo/CTXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CTXAppDelegate.h
+//  librabbitmq-objc-demo
+//
+//  Created by Alberto De Bortoli on 13/01/2014.
+//  Copyright (c) 2014 EF Education First. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
