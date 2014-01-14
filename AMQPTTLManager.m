2ac350676e5a7f8ab33886562a54ae3d7a7f93af
Alberto De Bortoli <albertodebortoli@gmail.com>
2014-01-14 13:51:41 +0000

    Updated AMQPTTLManager, thread-safe, based on dispatch_source. Fixed crash due to [IOS dispatch source retain] message sent to deallocated instance.

--- AMQPTTLManager.m
+++ AMQPTTLManager.m
@@ -2,7 +2,7 @@
 //  AMQPTTLManager.m
 //  librabbitmq-objc
 //
-//  Created by Pedro Gomes on 29/11/2012.
+//  Created by Alberto De Bortoli on 14/01/2014.
 //  Copyright (c) 2012 EF Education First. All rights reserved.
 //
 
@@ -22,31 +22,25 @@
 
 - (void)dealloc
 {
+    [self _performCleanup];
 #if RABBITMQ_DISPATCH_RETAIN_RELEASE
     dispatch_release(_lockQueue);
 #endif
-    [self _performCleanup];
 }
 
 - (id)initWithDelegate:(id<AMQPTTLManagerDelegate>)delegate
 {
-    self = [self init];
-    if (self) {
+    if ((self = [self init])) {
         _delegate = delegate;
     }
     return self;
 }
 - (id)init
 {
-    self = [super init];
-    if (self) {
+    if ((self = [super init])) {
+        _lockQueue  = dispatch_queue_create("com.librabbitmq-objc.amqp.ttlmanager.lock", NULL);
         _objects    = [[NSMutableArray alloc] init];
         _timers     = [[NSMutableArray alloc] init];
-
-        _lockQueue  = dispatch_queue_create("com.librabbitmq-objc.amqp.ttlmanager.lock", NULL);
-#if RABBITMQ_DISPATCH_RETAIN_RELEASE
-        dispatch_retain(_lockQueue);
-#endif
     }
     return self;
 }
@@ -70,10 +64,17 @@
 #endif
         dispatch_source_set_timer(timer, dispatch_time(DISPATCH_TIME_NOW, ttl * NSEC_PER_SEC), DISPATCH_TIME_FOREVER, 0);
         dispatch_source_set_event_handler(timer, ^{
-            [self _cancelTimerForObject:object];
+            dispatch_source_cancel(timer);
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self.delegate ttlForObjectExpired:object];
             });
+        });
+        dispatch_source_set_cancel_handler(timer, ^{
+            NSUInteger indexOfObject = [_objects indexOfObject:object];
+            if (indexOfObject != NSNotFound) {
+                [_timers removeObjectAtIndex:indexOfObject];
+                [_objects removeObject:object];
+            }
         });
         dispatch_resume(timer);
     });
@@ -96,48 +97,32 @@
 - (void)removeObject:(id)object
 {
     dispatch_sync(_lockQueue, ^{
-        [self _cancelTimerForObject:object];
+        NSUInteger indexOfObject = [_objects indexOfObject:object];
+        if (indexOfObject != NSNotFound) {
+            dispatch_source_t timer = [[_timers objectAtIndex:indexOfObject] pointerValue];
+            dispatch_source_cancel(timer);
+        }
     });
 }
 
 - (void)removeAllObjects
 {
-    dispatch_sync(_lockQueue, ^{ @autoreleasepool {
-            NSArray *objectsToRemove = [NSArray arrayWithArray:_objects];
-            [objectsToRemove enumerateObjectsUsingBlock:^(id object, NSUInteger idx, BOOL *stop) {
-                [self _cancelTimerForObject:object];
-            }];
-        }
+    dispatch_sync(_lockQueue, ^{
+        NSArray *objectsToRemove = [_objects copy];
+        [objectsToRemove enumerateObjectsUsingBlock:^(id object, NSUInteger idx, BOOL *stop) {
+            [self removeObject:object];
+        }];
     });
 }
 
 #pragma mark - Private Methods
 
-- (void)_cancelTimerForObject:(id)object
-{
-    NSUInteger indexOfObject = [_objects indexOfObject:object];
-    
-    if (indexOfObject == NSNotFound) {
-        return;
-    }
-    
-    dispatch_source_t timer = [[_timers objectAtIndex:indexOfObject] pointerValue];
-    dispatch_source_cancel(timer);
-    
-    [_objects removeObjectAtIndex:indexOfObject];
-    [_timers removeObjectAtIndex:indexOfObject];
-}
-
 - (void)_performCleanup
 {
     dispatch_sync(_lockQueue, ^{
-        @autoreleasepool {
-            [_timers enumerateObjectsUsingBlock:^(NSValue *timer, NSUInteger idx, BOOL *stop) {
-                dispatch_source_cancel([timer pointerValue]);
-            }];
-            [_timers removeAllObjects];
-            [_objects removeAllObjects];
-        }
+        [_timers enumerateObjectsUsingBlock:^(NSValue *timer, NSUInteger idx, BOOL *stop) {
+            dispatch_source_cancel([timer pointerValue]);
+        }];
     });
 }
 
