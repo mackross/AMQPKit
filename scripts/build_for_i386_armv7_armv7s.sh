19088bcd6655901f8b2f6cb963e814ca5bca02d2
Alberto De Bortoli <albertodebortoli@gmail.com>
2014-01-17 15:34:47 +0000

    Added script for building rabbitmq-c library for i386, armv7, armv7s.
    P.S. Working with Xcode5 tools installed.
    rabbitmq-c library: https://github.com/alanxz/rabbitmq-c/releases

---
+++ scripts/build_for_i386_armv7_armv7s.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo "Will build i386 rabbitmq-c library for iOS Simulator"
+make clean
+
+./configure --host=i386-apple-darwin --with-ssl=no --enable-static \
+  CC="/usr/bin/clang -arch i386" \
+  LD=$DEVROOT/usr/bin/ld
+
+make
+lipo -info librabbitmq/.libs/librabbitmq.a
+mv librabbitmq/.libs/librabbitmq.a librabbitmq.a.i386
+
+echo "Will build armv7 rabbitmq-c library for iOS Devices"
+make clean
+
+DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
+SDKROOT=$DEVROOT/SDKs/iPhoneOS7.0.sdk
+./configure --host=armv7-apple-darwin --enable-static --with-ssl=no \
+  CC="/usr/bin/clang -arch armv7" \
+  CPPFLAGS="-I$SDKROOT/usr/include/" \
+  CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+  LD=$DEVROOT/usr/bin/ld
+
+make
+lipo -info librabbitmq/.libs/librabbitmq.a
+mv librabbitmq/.libs/librabbitmq.a librabbitmq.a.armv7
+
+echo "Will build armv7s rabbitmq-c library for iOS Devices"
+make clean
+
+DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
+SDKROOT=$DEVROOT/SDKs/iPhoneOS7.0.sdk
+./configure --host=armv7s-apple-darwin --enable-static --with-ssl=no \
+  CC="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -arch armv7s" \
+  CPPFLAGS="-I$SDKROOT/usr/include/" \
+  CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT" \
+  LD=$DEVROOT/usr/bin/ld
+
+make
+lipo -info librabbitmq/.libs/librabbitmq.a
+mv librabbitmq/.libs/librabbitmq.a librabbitmq.a.armv7s
+
+echo "Will merge libs"
+
+DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer
+$DEVROOT/usr/bin/lipo -arch armv7 librabbitmq.a.armv7 -arch armv7s librabbitmq.a.armv7s -arch i386 librabbitmq.a.i386 -create -output librabbitmq.a
+file librabbitmq.a
